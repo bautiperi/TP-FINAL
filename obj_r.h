762cabeb28f9437f3b9c8f5529d978c802b7b774
Paloma Porco Paneiva <palomaporcopaneiva@Palomas-MacBook-Pro.local>
2024-01-05 11:36:11 -0300

    agregamos funciones para la visualizacion del display

--- obj_r.h
+++ obj_r.h
@@ -5,6 +5,26 @@
 #include "libs/disdrv.h"
 #include "libs/joydrv.h"
 
+/***********************************  GAMER   ***********************************/
+
+/*FUNCION GAMER_VIS
+brief: se encarga de mostrar al jugador
+ubi: (dcoord_t) coordenadas del LED de abajo a la izq
+return: void
+*/
+void gamer_vis(dcoord_t ubi);
+
+/***********************************  ALIENS   ***********************************/
+
+/*******************************  THE FINAL BOSS  *********************************/
+
+/*FUNCION FINAL_BOSS_VIS
+brief: se encarga de mostrar al final boss
+ubi: (dcoord_t) coordenadas del LED de arriba a la izq
+return: void
+*/
+void final_boss_vis(dcoord_t ubi);
+
 /***********************************  LIVES   ***********************************/
 
 /*FUNCION SHIELDS_VIS
@@ -17,18 +37,23 @@ void lives_vis(int cant);
 /***********************************  SHIELDS  ***********************************/
 
 /*FUNCION SHIELDS_VIS
-brief: se encarga de encender un escudo
+brief se encarga de encender un escudo
 coor: (dcoord_t) coordenada del led de abajo a la izquierda del shield
 return: void
 */
 void shields_vis(dcoord_t coor);
 
 /*FUNCION SHIELDS_LIFE
-brief: se encarga de las vidas de los escudos
-life_of_shield: (int) numero de colisiones hacia ese escudo (a las 10 colisiones se muere el escudo)
+brief: se encarga de la visualizacion de colisiones con los escudos
+life_of_shield: (int) numero de colisiones hacia ese escudo y ese LED (a las 8 colisiones se muere el escudo, cada LED tiene 2 vidas)
 coor_of_shield: (dcoord_t) escudo al que se le pego
 return: void
 */
 void shields_life(int life_of_shield, dcoord_t coor_of_shield);
+
+
+#define DELAY 300000 //0.3 segundos
+
+#define MAX_ITERATIONS 50
 
 #endif
