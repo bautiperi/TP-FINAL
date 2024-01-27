2c62d92d23adc8d14a89c34d1996b46ef792d2e6
camilacas <73140099+camilacas@users.noreply.github.com>
2024-01-27 19:06:04 -0300

    prueba

--- back_game_a.c
+++ back_game_a.c
@@ -42,7 +42,7 @@ void *alien_movement(void *arg)
 
             for (y = 1; y < FIL; y++)
             {
-                for (x = 0; x < COL; x +=)
+                for (x = 0; x < COL; x += 2)
                 {
                     // Analiza si se llegó al extremo de la matriz, para evitar que los enemigos se "amontonen"
                     if (enemy_checker(COL - 2, y, mapa))
