8d41a9540b8340de15997a827555dac8b7aaf425
camilacas <73140099+camilacas@users.noreply.github.com>
2024-01-27 18:35:56 -0300

    algo

--- back_score_a.h
+++ back_score_a.h
@@ -8,7 +8,7 @@
 #ifndef BACK_SCORE_A_H_
 #define BACK_SCORE_A_H_
 
-#include "defines.h"
+#include "_defines.h"
 
 /* FUNCIÓN SCORE_UPDATER
  * BRIEF: Actualiza el score actual del jugador
@@ -17,13 +17,26 @@
  */
 void score_updater(int mapa[][COL], int idetifier);
 
-/* FUNCIÓN GAMER_MOVEMENT
+/* FUNCIÓN SCORE_SAVER
  * BRIEF: Guarda el score del jugador en un archivo, ordenandolo de mayor a menor puntaje
- * mapa: (matriz de ints) Es la matriz donde se desarrolla el juego
- * return: (void)
+ * score: (int) Es el score del juego actual
+ * return: (int) Devuelve 0 si no hubo errores y se pudo guardar el score del jugador, -1 caso contrario
  */
-void score_saver(int mapa[][COL]);
+int score_saver(int score);
 
-char name[50] = "";
+/* FUNCIÓN LIFE_UPDATER
+ * BRIEF: Actualiza la cantidad de vidas que tiene el jugador
+ * mapa: (matriz de inta) Es la matriz donde se desarrolla el juego
+ * return: (int) Devuelve 0 si el jugador todavía tiene vidas, caso contrario devuelve 1
+ */
+int life_updater(int mapa[][COL]);
+
+char name[50];
+
+typedef struct
+{
+    char name[50];
+    int score;
+} player_t;
 
 #endif /* BACK_STORE_A_H_ */
