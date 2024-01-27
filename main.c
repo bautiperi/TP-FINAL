9f278a2b01ff07b658ec6704eb616357a6d40126
bauti <bauti@bauti>
2024-01-27 00:38:44 -0300

    Se agrega la función score saver que guarda el score del jugador en caso de ser lo suficientemente alto para entrar al top 10.

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ int main(void){
 
 			map_def(EASY, mapa);
 
-			pthread_t * up_aliens, * up_boss, *up_player;
+			pthread_t up_aliens, up_boss, up_player;
 
 			pthread_create(&up_boss, NULL, final_boss_creation, mapa);
 			pthread_create(&up_aliens, NULL, alien_movement, mapa);
