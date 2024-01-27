2282ffc2082536fb2c4dfddbb2325f1766ccb630
camilacas <73140099+camilacas@users.noreply.github.com>
2024-01-27 18:13:57 -0300

    algo

--- back_game_a.c
+++ back_game_a.c
@@ -40,7 +40,7 @@ void alien_movement(int mapa[][COL])
                 for (x = 0; x < COL; x++)
                 {
                     // Analiza si se llegó al extremo de la matriz, para evitar que los enemigos se "amontonen"
-                    if (mapa[y][COL - 2] == 4)
+                    if (mapa[y][COL - 2] == 4 && mapa[y][COL - 2] == 3 && mapa[y][COL - 2] == 2)
                     {
                         dir = -1; // Hace el cambio de dirección
                         flag = 1; // Hace que al terminar de cambiar el resto de las filas, se llame a la función para el cambio vertical
@@ -68,7 +68,7 @@ void alien_movement(int mapa[][COL])
                 for (x = COL - 1; x >= 0; x--)
                 {
                     // Analiza si se llegó al extremo de la matriz, para evitar que los enemigos se "amontonen"
-                    if (mapa[y][0] == 4)
+                    if (mapa[y][COL - 2] == 4 && mapa[y][COL - 2] == 3 && mapa[y][COL - 2] == 2)
                     {
                         dir = 1;  // Hace el cambio de dirección
                         flag = 1; // Hace que al terminar de cambiar el resto de las filas, se llame a la función para el cambio vertical
