5e6719438a4d6c3def22b48171a9ee5fa85b867d
camilacas <73140099+camilacas@users.noreply.github.com>
2024-01-28 15:45:48 -0300

    prueba

--- back_game_a.c
+++ back_game_a.c
@@ -44,7 +44,7 @@ void *alien_movement(void *arg)
                 for (x = 0; x < COL; x++)
                 {
                     // Analiza si se llegó al extremo de la matriz, para evitar que los enemigos se "amontonen"
-                    if (enemy_checker(COL - 1, y, mapa))
+                    if (enemy_checker(COL - 1, y, mapa) || enemy_checker(0, y, mapa))
                     {
                         dir *= -1; // Hace el cambio de dirección
                         flag = 1;  // Hace que al terminar de cambiar el resto de las filas, se llame a la función para el cambio vertical
