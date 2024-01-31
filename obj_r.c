92510b067a79940b354f8d47e8f570cc539533fb
camilacas <73140099+camilacas@users.noreply.github.com>
2024-01-30 23:36:29 -0300

    correccion de errores

--- obj_r.c
+++ obj_r.c
@@ -16,7 +16,7 @@ void gamer_vis(dcoord_t ubi)
     int i;
     // coordenadas de lo que se tiene que prender
     dcoord_t arr[4] = {{ubi.x, ubi.y}, {ubi.x, ubi.y--}, {ubi.x--, ubi.y++}, {(ubi.x += 2), ubi.y}};
-    for (i = 0; i <= 4; i++) // prende todas las coordenadas
+    for (i = 0; i < 4; i++) // prende todas las coordenadas
     {
         disp_write(arr[i], D_ON);
     }
@@ -98,7 +98,7 @@ void final_boss_vis(dcoord_t ubi, int mapa[][COL])
         int i;
         // coordenadas de lo que se tiene que prender
         dcoord_t arr[4] = {{ubi.x, ubi.y}, {ubi.x, ubi.y++}, {ubi.x--, ubi.y--}, {ubi.x += 2, ubi.y}};
-        for (i = 0; i <= 4; i++) // prende todas las coordenadas
+        for (i = 0; i < 4; i++) // prende todas las coordenadas
         {
             disp_write(arr[i], D_ON);
         }
