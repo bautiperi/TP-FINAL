0cc296bd753cf087dce029892d8350635f3c79c2
camilacas <73140099+camilacas@users.noreply.github.com>
2024-02-02 22:19:32 -0300

    prueba

--- main.c
+++ main.c
@@ -54,7 +54,7 @@ int main(void)
 		while (STATUS == 0) // JUEGO
 		{
 			coord = joy_read();
-			usleep(50000);
+			usleep(200000);
 			disp_write(coord_menu, D_ON); // coordenada que simula un "boton" de menu
 
 			if ((coord.sw) == (J_PRESS)) // dispara el player
