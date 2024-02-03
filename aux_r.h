9bf670933959431b2095711ef7c966664060b807
cam-castro <155557306+cam-castro@users.noreply.github.com>
2024-02-03 19:27:37 -0300

    Update aux_r.h

--- aux_r.h
+++ aux_r.h
@@ -9,4 +9,12 @@ return: void
 */
 void shutdown_disp(void);
 
+/***********************************  GAME OVER   ***********************************/
+
+/*FUNCION GAME_OVER
+brief: apaga los leds del display y genera una cara triste indicando la finalizacion del juego
+no recibe ni devuleve nada
+*/
+void game_over(void);
+
 #endif /* AUX_R_H */
