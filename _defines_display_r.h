87ffc8b566d0278a06774c6b68421c9713ffbf5f
palomonserrat <92894468+palomonserrat@users.noreply.github.com>
2024-02-03 16:23:18 -0300

    Update _defines_display_r.h

--- _defines_display_r.h
+++ _defines_display_r.h
@@ -27,6 +27,19 @@
 #define DIFFICULTY 3
 #define QUIT 4
 
+// DEFINES TIEMPOS DE FLICKS
+#define DELAY 300000 // 0.3 segundos
+#define MAX_ITERATIONS 50
+
+// JOYSTICK
+#define THRESHOLD 40 // Límite a partir del cual se mueve el LED encendido
+
+// FLAGS
+#define STATUS mapa[0][4]
+#define FLICK_OPTION mapa[0][5]
+#define DIRECCION mapa[0][6]
+#define CLOSE_GAME mapa[6][0]
+
 #endif /* _DEFINES_DISPLAY_R_H */
 
 /*
